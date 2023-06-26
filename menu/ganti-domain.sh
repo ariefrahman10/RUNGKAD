d782eb0076c53d68c40363b0b020b5aa2a0e44ff
ariefrahman10 <120597758+ariefrahman10@users.noreply.github.com>
2023-06-26 11:52:21 +0700

    Update ganti-domain.sh

--- menu/ganti-domain.sh
+++ menu/ganti-domain.sh
@@ -61,3 +61,28 @@ bash acme.sh --installcert -d $domain1 --fullchainpath /etc/xray/xray.crt --keyp
 sleep 3
 clear
 restart-xray
+
+echo start
+sleep 0.5
+source /var/lib/akbarstorevpn/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+# Delete Files
+rm -f /etc/xray/xray.crt
+rm -f /etc/xray/xray.key
+
+systemctl enable xray.service
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+curl https://get.acme.sh | sh
+bash acme.sh --install
+cd .acme.sh
+bash acme.sh --set-default-ca --server letsencrypt
+bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --issue --standalone -d $domain--force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+sleep 3
+restart-xray
