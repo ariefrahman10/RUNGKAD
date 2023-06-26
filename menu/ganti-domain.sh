4890b47a095e4b3a50681607aff104f46f9b91a2
ariefrahman10 <120597758+ariefrahman10@users.noreply.github.com>
2023-06-26 11:50:17 +0700

    Update ganti-domain.sh

--- menu/ganti-domain.sh
+++ menu/ganti-domain.sh
@@ -1,23 +1,63 @@
-#!/bin/bash
-# Input Domain
-clear
-echo "Input Domain"
-echo " "
-read -rp "Input domain kamu : " -e dns
-if [ -z $dns ]; then
-echo -e "Nothing input for domain!"
+#Domain ama
+source /var/lib/akbarstorevpn/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
 else
-echo "$dns" > /usr/local/etc/xray/domain
+domain=$IP
 fi
+clear
+echo -e "========================="
+read -rp "Input Domain/Host : " -e domain1
+echo -e "========================="
+echo -e "Checking Domain: ${BLUE}${domain1} ${NC}Please Wait..."
+echo -e "========================="
+sleep 3
 
-# Install Cert
-systemctl stop nginx
-domain=$(cat /usr/local/etc/xray/domain)
+# Cek DNS terubung dengan VPS atau tidak
+# cekdomain=$(curl -sm8 http://ipget.net/?ip="${domain1}")
+# if [[ ${MYIP} == ${cekdomain} ]]; then
+#     echo -e "${success}Domain: ${BLUE}${domain1} ${NC}Terhubung dengan IP VPS"
+#     sleep 3
+#     clear
+# else
+#     echo -e "${error1}Domain: ${RED}${domain1} ${NC}Tidak Terhubung dengan IP VPS"
+#     sleep 3
+#     exit 0
+# fi
+# done
+# Delete Files
+rm -f /etc/xray/xray.crt
+rm -f /etc/xray/xray.key
+rm /root/domain
+rm -f /etc/xray/domain
+# Done
+echo $domain1 >> /etc/xray/domain
+echo $domain1 >> /root/domain
+sed -i "s/IP=${domain}/IP=${domain1}/g" /var/lib/akbarstorevpn/ipvps.conf
+#Bersihkan terminal
+clear
+sleep 1
+
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
+ntpdate pool.ntp.org
+apt -y install chrony
+timedatectl set-ntp true
+systemctl enable chronyd && systemctl restart chronyd
+systemctl enable chrony && systemctl restart chrony
+timedatectl set-timezone Asia/Jakarta
+chronyc sourcestats -v
+chronyc tracking -v
+mkdir -p /etc/xray
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root
 curl https://get.acme.sh | sh
-source ~/.bashrc
+bash acme.sh --install
 cd .acme.sh
-bash acme.sh --issue -d $domain --server letsencrypt --keylength ec-256 --fullchain-file /usr/local/etc/xray/xray.crt --key-file /usr/local/etc/xray/xray.key --standalone --force
-systemctl restart nginx
-echo "Pergantian Domain Selesai"
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+bash acme.sh --set-default-ca --server letsencrypt
+bash acme.sh --register-account -m senowahyu62@gmail.com
+bash acme.sh --issue -d $domain1 --standalone
+bash acme.sh --installcert -d $domain1 --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+sleep 3
+clear
+restart-xray
